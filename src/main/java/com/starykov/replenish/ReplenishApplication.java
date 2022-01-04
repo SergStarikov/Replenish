34da54559d28344e86906060313af45107f06a81
SerhiiStarykov <serhii.starykov@sigma.software>
2022-01-04 20:01:51 +0200

    Initial commit

---
+++ src/main/java/com/starykov/replenish/ReplenishApplication.java
@@ -0,0 +1,16 @@
+package com.starykov.replenish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class ReplenishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplenishApplication.class, args);
+        new HashMap<String,Integer>();
+    }
+
+}
---
+++ src/test/java/com/starykov/replenish/ReplenishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starykov.replenish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplenishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
